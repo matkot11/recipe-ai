a6017e2001f3748ad86007c429fe1e2657d863ac
Mateusz Kocik <matkot11@gmail.com>
2026-04-16 10:04:03 +0200

    Revert "chore: scaffold Nuxt project files, add AGENTS.md"
    This reverts commit 548ff69b62c6ab8d5357f1a3541bcc8f9880dc93.

--- tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./components/**/*.{vue,js,ts}', './layouts/**/*.vue', './pages/**/*.vue', './app.vue', './plugins/**/*.{js,ts}'],
-  theme: { extend: {} },
-  plugins: []
-}
